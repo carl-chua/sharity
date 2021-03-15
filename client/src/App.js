3e78dae775af0c7c90fc43e3df10904e1bfff2c1
carl-chua <carlchua0701@gmail.com>
2021-03-15 14:24:58 +0800

    slight formatting

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,12 @@ import Charity from "./contracts/Charity.json";
 import Donation from "./contracts/Donation.json";
 
 class App extends Component {
-  state = { web3: null, accounts: null, charityContract: null, donationContract: null };
+  state = {
+    web3: null,
+    accounts: null,
+    charityContract: null,
+    donationContract: null,
+  };
 
   componentDidMount = async () => {
     try {
@@ -24,22 +29,27 @@ class App extends Component {
       const deployedNetworkCharity = Charity.networks[networkId];
       const charityInstance = new web3.eth.Contract(
         Charity.abi,
-        deployedNetworkCharity && deployedNetworkCharity.address,
+        deployedNetworkCharity && deployedNetworkCharity.address
       );
 
       const deployedNetworkDonation = Donation.networks[networkId];
       const donationInstance = new web3.eth.Contract(
         Donation.abi,
-        deployedNetworkDonation && deployedNetworkDonation.address,
+        deployedNetworkDonation && deployedNetworkDonation.address
       );
-      
+
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, charityContract: charityInstance, donationContract: donationInstance });
+      this.setState({
+        web3,
+        accounts,
+        charityContract: charityInstance,
+        donationContract: donationInstance,
+      });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contracts. Check console for details.`,
+        `Failed to load web3, accounts, or contracts. Check console for details.`
       );
       console.error(error);
     }
@@ -48,10 +58,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      {!this.state.web3 || this.state.isError === true 
-        ? <ErrorPage />
-        : <HomePage web3={this.state.web3} accounts={this.state.accounts} charityContract={this.state.charityContract} donationContract={this.state.donationContract}/>}
-    </div>
+        {!this.state.web3 || this.state.isError === true ? (
+          <ErrorPage />
+        ) : (
+          <HomePage
+            web3={this.state.web3}
+            accounts={this.state.accounts}
+            charityContract={this.state.charityContract}
+            donationContract={this.state.donationContract}
+          />
+        )}
+      </div>
     );
   }
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render(){
-        return (
-            <div>
-                <div className="tc">
-                    <h1 className="f2">Hi</h1>
-                </div>
-            </div>
-        )
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="tc">
+          <h1 className="f2">Hi</h1>
+          {console.log(this.props)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
