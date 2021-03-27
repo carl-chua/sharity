35c8de9ecbe9ff08bd1d520c0f535c9542959d87
Xin Ying <e0323888@u.nus.edu>
2021-03-27 14:33:09 +0800

    commit before merge

--- client/src/pages/RegisterCharity.jsx
+++ client/src/pages/RegisterCharity.jsx
@@ -27,12 +27,15 @@ class RegisterCharity extends React.Component {
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  /*componentDidMount() {
-    const getNumber = async() => {
-        this.state.charityId = await this.props.charityContract.methods.getNoOfCharities().call();
-    }
-    getNumber()
-  }*/
+  componentDidMount() {
+    const getNumber = async () => {
+      this.state.charityId = await this.props.charityContract.methods
+        .getNoOfCharities()
+        .call();
+      console.log(this.state.charityId);
+    };
+    getNumber();
+  }
 
   handleInputChange(event) {
     const target = event.target;
@@ -46,32 +49,49 @@ class RegisterCharity extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(this.state);
-    /*try {
-            this.props.charityContract.methods.registerCharity(this.state.name, this.state.description, this.state.avatarURL,this.state.contact, this.state.address).send({from: this.props.accounts[0]})
-            .on("receipt", (receipt) => {
-                console.log(receipt)
-                alert("Registration successful, please wait for Sharity to verify your registration")
-            })
-            .on("error", error => {
-                console.log(error.message);
-                alert("Registration unsuccessful, please register again. Error Occured: " + error.message)
-            });
-        } catch (err) {
-            console.log(err)
-        }*/
-    alert(
-      "Registration successful, please wait for Sharity to verify your registration"
-    );
-    //var newId = getNum();
-    var newId = this.state.charityId + 1;
-    this.setState({
-      charityId: newId,
-      name: "",
-      description: "",
-      contact: "",
-      address: "",
-      avatarURL: "",
-    });
+    try {
+      this.props.charityContract.methods
+        .registerCharity(
+          this.state.name,          
+          this.state.address,
+          this.state.contact,
+          this.state.description,
+          this.state.avatarURL
+        )
+        .send({ from: this.props.accounts[0] })
+        .on("receipt", (receipt) => {
+          console.log(receipt);
+          var newId = this.state.charityId + 1;
+          console.log(newId);
+          this.setState({
+            charityId: newId,
+            name: "",
+            description: "",
+            contact: "",
+            address: "",
+            avatarURL: "",
+          });
+          alert(
+            "Registration successful, please wait for Sharity to verify your registration"
+          );
+        })
+        .on("error", (error) => {
+          console.log(error.message);
+          this.setState({
+            name: "",
+            description: "",
+            contact: "",
+            address: "",
+            avatarURL: "",
+          });
+          alert(
+            "Registration unsuccessful, please register again. Error Occured: " +
+              error.message
+          );
+        });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
