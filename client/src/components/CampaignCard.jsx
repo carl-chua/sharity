32ff08a8802e16e7f3976a65ee752249af87ee93
carl-chua <carlchua0701@gmail.com>
2021-04-09 02:35:30 +0800

    Fixed frontend bytes to string

--- client/src/components/CampaignCard.jsx
+++ client/src/components/CampaignCard.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import { useHistory, Link } from "react-router-dom";
